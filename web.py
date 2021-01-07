b28a3ff76fc6ce8191994c65498dd1782567e7f4
Vinícius <vinybf@hotmail.com>
2021-01-07 14:01:47 -0300

    agr coleta restaurantes

--- web.py
+++ web.py
@@ -63,6 +63,46 @@ def get_data_hotel(entry_link):
     #print(data)
     return data 
 
+def get_data_restaurant(entry_link):
+    entry_url = 'https://www.tripadvisor.com.br/' + entry_link
+    r = requests.get(entry_url)
+    soup = BeautifulSoup(r.text, 'html.parser')
+    nome = soup.find('h1', {'data-test-target':'top-info-header'}).string
+    try:
+        endereco = soup.find('a', {'class' : '_15QfMZ2L', 'href': '#MAPVIEW'}).string.replace(',',' |')
+    except:
+        endereco = 'indef'
+    try:
+        avaliacoes = soup.find('a', class_='_10Iv7dOs').string.split()[0]
+    except:
+        avaliacoes = 'indef'
+    try:
+        nota = soup.find('span', class_='r2Cf69qf').text.strip().replace(',','.')
+    except:
+        nota = 'indef'
+    try:
+        faixa_preco = soup.find('div', class_='_1XLfiSsv').string.split('-')
+        preco_min = faixa_preco[0].split()[1]
+        preco_max = faixa_preco[1].split()[1]
+    except:
+        preco_min = 'indef'
+        preco_max = 'indef'
+    try:
+        restaurant_id = entry_link.split('-')[2][1:]
+        api_url = 'https://www.tripadvisor.com.br/data/1.0/mapsEnrichment/restaurant/{}'.format(restaurant_id)
+        api_json = requests.get(api_url).json()
+        coords = api_json['geoPoint']
+        lat = str(coords['latitude'])
+        lon = str(coords['longitude'])
+    except:
+        lat = 'indef'
+        lon = 'indef'
+    
+    data = [nome, endereco, nota, avaliacoes, preco_min, preco_max, lat, lon, entry_url]
+    print(data)
+    return data
+    
+
 #Infere o tipo a partir do nome do hotel
 def get_type_by_name(name, possible_types):
     for tipo in possible_types:
@@ -85,6 +125,17 @@ def get_hotel_links(url):
         titles_links.append(title_link)
 
     return titles_links
+
+def get_restaurants_links(url):
+    r = requests.get(url)
+    soup = BeautifulSoup(r.text, 'html.parser')
+    restaurant_items = soup.findAll(class_='_1llCuDZj')
+    restaurant_links = []
+    for restaurant in restaurant_items:
+        restaurant_link = restaurant.find('a')['href']
+        restaurant_links.append(restaurant_link)
+    
+    return restaurant_links
 
 #Gera, a partir de uma URl inicial, as URLS correspondentes ao avançar uma página na
 #listagem
@@ -119,7 +170,6 @@ def get_max_num_pages(url):
 def coleta_dados(initial_url, url_to_offset, get_links_function, data_extractor_function):
     num_pages = get_max_num_pages(initial_url)
     page_urls = get_page_urls(initial_url, url_to_offset, num_pages, 30)
-    print(page_urls)
 
     data = []
     #Seria bom utilizar multiprocessamento aqui
@@ -130,13 +180,18 @@ def coleta_dados(initial_url, url_to_offset, get_links_function, data_extractor_
     return data
 
 if __name__ == "__main__":
+    '''
     headers_hotel = ["nome", "endereço", "tipo", "qtd_quartos", "qtd_avaliacoes", "nota", "categoria", 
                 "nota_pedesrtres", "restaurantes_perto", "atracoes_perto", "latitude", "longitude", "fonte"]
 
     hotel_initial_url = 'https://www.tripadvisor.com.br/Hotels-g303389-Ouro_Preto_State_of_Minas_Gerais-Hotels.html'
     hotel_url_to_offset = 'https://www.tripadvisor.com.br/Hotels-g303389-oa{}-Ouro_Preto_State_of_Minas_Gerais-Hotels.html'
 
-    data = coleta_dados(hotel_initial_url, hotel_url_to_offset, get_hotel_links, get_data_hotel)
+    data_hoteis = coleta_dados(hotel_initial_url, hotel_url_to_offset, get_hotel_links, get_data_hotel)
+    write_to_file("hoteis.csv", headers_hotel, data_hoteis)
+    print(f'{len(data_hoteis)} hotéis coletados')
+    '''
 
-    write_to_file("hoteis.csv", headers_hotel, data)
-    print(f'{len(data)} hotéis coletados')
+    restaurant_initial_url = 'https://www.tripadvisor.com.br/Restaurants-g303389-Ouro_Preto_State_of_Minas_Gerais.html'
+    restaurant_url_to_offset = 'https://www.tripadvisor.com.br/RestaurantSearch-g303389-oa{}-Ouro_Preto_State_of_Minas_Gerais.html#EATERY_LIST_CONTENTS'
+    data_restaurantes = coleta_dados(restaurant_initial_url, restaurant_url_to_offset, get_restaurants_links, get_data_restaurant)
