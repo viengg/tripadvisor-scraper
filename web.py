6e6651b7b98078aad7fbb6bba16aadefa11a4a7f
Vinícius <vinybf@hotmail.com>
2021-01-11 18:54:57 -0300

    coleta mariana e ouro preto

--- web.py
+++ web.py
@@ -1,14 +1,17 @@
 from bs4 import BeautifulSoup
 import requests
 import json
+import time
 
 #A partir de um link de hotel, entra na pagina do hotel em questão
 #e extrai seus dados
 def get_hotel_data(entry_link):
+    time.sleep(1)
     entry_url = 'https://www.tripadvisor.com.br' + entry_link
     r = requests.get(entry_url)
     soup = BeautifulSoup(r.text, 'html.parser')
     nome = soup.find(id="HEADING").string.strip()
+    cidade = soup.find('a', id='global-nav-tourism').string
 
     try:
         endereco = soup.find(class_='_3ErVArsu jke2_wbp').string.replace(","," |")
@@ -59,6 +62,7 @@ def get_hotel_data(entry_link):
     data ={
         'nome': nome,
         'endereco': endereco,
+        'cidade': cidade,
         'tipo': tipo,
         'qtd_quartos': qtd_quartos,
         'qtd_avaliacoes': qtd_avaliacoes,
@@ -76,10 +80,13 @@ def get_hotel_data(entry_link):
 
 #A partir de um link de restaurante, entra na pagina e extrai os seus dados
 def get_restaurante_data(entry_link):
+    time.sleep(1)
     entry_url = 'https://www.tripadvisor.com.br' + entry_link
     r = requests.get(entry_url)
     soup = BeautifulSoup(r.text, 'html.parser')
     nome = soup.find('h1', {'data-test-target':'top-info-header'}).string.replace(',', ' |')
+    cidade = soup.find('a', id='global-nav-tourism').string
+
     try:
         endereco = soup.find('a', {'class' : '_15QfMZ2L', 'href': '#MAPVIEW'}).string.strip().replace(',',' |')
     except:
@@ -106,6 +113,7 @@ def get_restaurante_data(entry_link):
     data = {
         'nome': nome,
         'endereco': endereco,
+        'cidade': cidade,
         'nota': nota,
         'qtd_avaliacoes': avaliacoes,
         'latitude': lat,
@@ -117,10 +125,16 @@ def get_restaurante_data(entry_link):
 
 #A partir de um link de atracao, entra na pagina e extrai seus dados
 def get_atracao_data(entry_link):
+    time.sleep(1)
     entry_url = 'https://www.tripadvisor.com.br' + entry_link
     r = requests.get(entry_url)
     soup = BeautifulSoup(r.text, 'html.parser')
-    nome = soup.find('h1', id='HEADING').string.strip()
+    try:
+        nome = soup.find('h1', id='HEADING').string.strip()
+    except:
+        nome = 'indef'
+    cidade = soup.find('a', id='global-nav-tourism').string
+
     try:
         endereco = soup.find('div', class_='LjCWTZdN').findAll('span')[1].string.replace(',', ' |')
     except:
@@ -148,6 +162,7 @@ def get_atracao_data(entry_link):
     data = {
         'nome': nome,
         'endereco': endereco,
+        'cidade': cidade,
         'nota': nota,
         'qtd_avaliacoes': avaliacoes,
         'latitude': lat,
@@ -229,10 +244,7 @@ def get_page_urls(initial_url, page_type):
 
 #Escreve os dados coletados em um arquivo .csv
 def write_to_file(filename, data):
-    with open(filename, "w") as f:
-        header = data[0].keys()
-        header_buffer = ",".join(header) + "\n"
-        f.write(header_buffer)
+    with open(filename, "a") as f:
         for instance in data:
             values = instance.values()
             buffer = ",".join(values)
@@ -258,20 +270,20 @@ def coleta_dados(initial_url, data_extractor, get_links, page_type):
 
     return data
 
-def coleta_hoteis_e_escreve(url):
-    data_hoteis = coleta_dados(url, get_hotel_data, get_hotel_links, 'hotel')
-    write_to_file("hoteis.csv", data_hoteis)
-    print(f'{len(data_hoteis)} hotéis coletados')
+def coleta_hoteis(url):
+    hoteis = coleta_dados(url, get_hotel_data, get_hotel_links, 'hotel')
+    write_to_file('hoteis.csv', hoteis)
+    print(f'\n{len(hoteis)} hoteis coletados\n')
 
-def coleta_restaurantes_e_escreve(url):
-    data_restaurantes = coleta_dados(url, get_restaurante_data, get_restaurante_links, 'restaurante')
-    write_to_file('restaurantes.csv', data_restaurantes)
-    print(f'{len(data_restaurantes)} restaurantes coletados')
+def coleta_restaurantes(url):
+    restaurantes = coleta_dados(url, get_restaurante_data, get_restaurante_links, 'restaurante')
+    write_to_file('restaurantes.csv', restaurantes)
+    print(f'\n{len(restaurantes)} restaurantes coletados\n')
 
-def coleta_atracoes_e_escreve(url):
-    data_atracoes = coleta_dados(url, get_atracao_data, get_atracao_links, 'atracao')
-    write_to_file('atracoes.csv', data_atracoes)
-    print(f'{len(data_atracoes)} atracoes coletadas')
+def coleta_atracoes(url):
+    atracoes = coleta_dados(url, get_atracao_data, get_atracao_links, 'atracao')
+    write_to_file('atracoes.csv', atracoes)
+    print(f'\n{len(atracoes)} atracoes coletadas\n')
 
 def get_links_from_city(city_url):
     r = requests.get(city_url)
@@ -287,9 +299,36 @@ def get_links_from_city(city_url):
 
 def coleta_por_cidade(city_url):
     hotel_url, restaurante_url, atracao_url = get_links_from_city(city_url)
-    coleta_hoteis_e_escreve(hotel_url)
-    coleta_restaurantes_e_escreve(restaurante_url)
-    coleta_atracoes_e_escreve(atracao_url)
+    coleta_hoteis(hotel_url)
+    coleta_restaurantes(restaurante_url)
+    coleta_atracoes(atracao_url)
+
+def coleta_cidades(city_url_list):
+    create_files()
+    for city_url in city_url_list:
+        coleta_por_cidade(city_url)
+
+def create_files():
+    hotel_header = ['nome', 'endereco', 'cidade', 'tipo', 'qtd_quartos',
+    'qtd_avaliacoes', 'nota', 'categoria', 'nota_pedestres',
+    'restaurantes_perto', 'atracoes_perto', 'latitude', 'longitude',
+    'fonte']
+    restaurante_header = ['nome', 'endereco', 'cidade', 'nota', 'qtd_avaliacoes',
+    'latitude', 'longitude', 'fonte']
+    atracao_header = ['nome', 'endereco', 'cidade', 'nota', 'qtd_avaliacoes',
+    'latitude', 'longitude', 'fonte']
+
+    with open('hoteis.csv','w') as f:
+        header_buffer = ",".join(hotel_header) + "\n"
+        f.write(header_buffer)
+    with open('restaurantes.csv', 'w') as f:
+        header_buffer = ",".join(restaurante_header) + "\n"
+        f.write(header_buffer)
+    with open('atracoes.csv', 'w') as f:
+        header_buffer = ",".join(atracao_header) + "\n"
+        f.write(header_buffer)
 
 if __name__ == "__main__":
-    coleta_por_cidade('https://www.tripadvisor.com.br/Tourism-g303389-Ouro_Preto_State_of_Minas_Gerais-Vacations.html')
+    cidades_url = ['https://www.tripadvisor.com.br/Tourism-g303389-Ouro_Preto_State_of_Minas_Gerais-Vacations.html', 
+    'https://www.tripadvisor.com.br/Tourism-g303386-Mariana_State_of_Minas_Gerais-Vacations.html']
+    coleta_cidades(cidades_url)
