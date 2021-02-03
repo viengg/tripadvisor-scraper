3cd224870f4604eef4db99a074cc3d4ac4ffd4a1
Vinícius <vinybf@hotmail.com>
2021-02-03 12:57:01 -0300

    remove dependencia re

--- web.py
+++ web.py
@@ -7,7 +7,6 @@ from functools import partial, reduce
 import os
 import dateparser
 from selenium import webdriver
-import re
 
 session = requests.Session()
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.2; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/32.0.1667.0 Safari/537.36'}
